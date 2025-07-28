5d8133d544c5d6b25bb96c072fd5d8ae76e8b2e3
SaintHelios <184778372+SaintHelios@users.noreply.github.com>
2025-07-28 22:49:46 +0300

    assignment 2 dun

---
+++ Assignment-02/ifc.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+int a = 5;
+float b = 2.2;
+char c = 'b';
+
+	printf("this is an int, it's value is %d\n", a);
+	printf("this is a float, it's value is %f\n", b);
+	printf("this is a char, it's value is %c\n", c);
+}
